c0fe486dd8da9fa5c3564b919f5e8b40c92d941e
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2022-11-02 11:31:40 +0100

    Add files via upload

---
+++ Buisness card reader.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from PIL import Image 
+import cv2
+import pytesseract
+from pytesseract import Output
+
+    
+image_file =st.file_uploader('Upload a Buisness Card',type=['png','jpeg','jpg'])
+if image_file  is not None:
+    image = Image.open(image_file)
+    d = pytesseract.image_to_data(image, output_type=Output.DICT)
+    
+    text=pytesseract.image_to_string(image)
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 60:
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            image = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    st.image(image)
+
